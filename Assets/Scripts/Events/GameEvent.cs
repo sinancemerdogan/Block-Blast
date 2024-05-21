11bf7e6f07d8f3bb50e679996af9224ae23a7c88
Sinan Cem Erdoğan <72517285+sinancemerdogan@users.noreply.github.com>
2024-05-21 10:53:09 +0300

    events

---
+++ Assets/Scripts/Events/GameEvent.cs
@@ -0,0 +1,50 @@
+﻿// ----------------------------------------------------------------------------
+// Unite 2017 - Game Architecture with Scriptable Objects
+// 
+// Author: Ryan Hipple
+// Date:   10/04/17
+// ----------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+
+[CreateAssetMenu(fileName = "Game Event", menuName = "Scriptable Objects/Game Event")]
+public class GameEvent : ScriptableObject {
+    /// <summary>
+    /// The list of listeners that this event will notify if it is raised.
+    /// </summary>
+    private readonly List<GameEventListener> eventListeners =
+        new List<GameEventListener>();
+
+    public void Raise() {
+        for (int i = eventListeners.Count - 1; i >= 0; i--)
+            eventListeners[i].OnEventRaised();
+    }
+
+    public void RegisterListener(GameEventListener listener) {
+        if (!eventListeners.Contains(listener))
+            eventListeners.Add(listener);
+    }
+
+    public void UnregisterListener(GameEventListener listener) {
+        if (eventListeners.Contains(listener))
+            eventListeners.Remove(listener);
+    }
+}
+[CustomEditor(typeof(GameEvent))]
+public class GameEventEditor : Editor {
+    public override void OnInspectorGUI() {
+        base.OnInspectorGUI();
+
+        GameEvent gameEvent = (GameEvent)target;
+
+        GUILayout.Space(10);
+
+        if (GUILayout.Button("Raise Event")) {
+            gameEvent.Raise();
+        }
+    }
+}
